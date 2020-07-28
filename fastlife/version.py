99589fd7af252388706b360244512345b82d589a
Benjamin Bengfort <benjamin@bengfort.com>
2020-07-27 21:36:15 -0400

    version bump to v0.2

--- fastlife/version.py
+++ fastlife/version.py
@@ -19,11 +19,11 @@ Maintains version and package information for deployment.
 
 __version_info__ = {
     "major": 0,
-    "minor": 1,
+    "minor": 2,
     "micro": 0,
     "releaselevel": "final",
     "post": 0,
-    "serial": 1,
+    "serial": 2,
 }
 
 ##########################################################################
